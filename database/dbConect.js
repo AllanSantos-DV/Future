891282c805c705d642bb8a3cb43a2ae08d894302
Allan Santos <allannascimentodossantos@gmail.com>
2024-04-04 23:45:45 -0300

    GitHub Actions config

--- database/dbConect.js
+++ database/dbConect.js
@@ -3,11 +3,6 @@ const env = dotenv.parsed;
 
 const Seq = require('sequelize');
 
-console.log(process.env.DB_DIALECT);  // Imprima o valor de DB_DIALECT para depuração
-console.log(process.env.DB_HOST);  // Imprima o valor de DB_HOST para depuração
-console.log(process.env.DB_NAME);  // Imprima o valor de DB_NAME para depuração
-console.log(process.env.DB_USER);  // Imprima o valor de DB_USER para depuração
-
 const db = new Seq(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
     host: env.DB_HOST,
     dialect: env.DB_DIALECT,
